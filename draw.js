3be352349ed48ee4d89693d1e01bb76fd127b4e5
ikwok <ikwok@wellesley.edu>
2013-11-17 16:23:41 -0500

    Testing star

--- draw.js
+++ draw.js
@@ -107,7 +107,30 @@ function draw(frame) {
 				if (gesture.type == "screenTap"){
 					console.log("SCREEN TAP");
 					ctx.beginPath();
-					ctx.arc(currentX,-currentY,8,0,2*Math.PI);
+					//ctx.arc(currentX,-currentY,8,0,2*Math.PI);
+					
+					//drawing star
+					length = 15;
+					ctx.rotate((Math.PI * 1/10));
+					// make a point, 5 times
+					for (var i = 5; i--;) {
+						// draw line up
+						ctx.lineTo(0, length);
+						// move origin to current same location as pen
+						ctx.translate(0, length);
+						// rotate the drawing board
+						ctx.rotate((Math.PI * 2 / 10));
+						// draw line down
+						ctx.lineTo(0, -length);
+						// again, move origin to pen...
+						ctx.translate(0, -length);
+						// ...and rotate, ready for next arm
+						ctx.rotate(-(Math.PI * 6 / 10));
+					}
+					// last line to connect things up
+					ctx.lineTo(0, length);
+					ctx.closePath();
+
 					ctx.stroke();
 					playNote(currentY);
 				}
